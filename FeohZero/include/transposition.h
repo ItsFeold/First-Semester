c5b9dc2b2c4ed9ac501a2200c43f4ccad797ce8a
Vlassis Maxoutis <74147053+ItsFeold@users.noreply.github.com>
2025-06-28 13:35:36 +0300

    Add files via upload

---
+++ FeohZero/include/transposition.h
@@ -0,0 +1,23 @@
+#ifndef TRANSPOSITION_H
+#define TRANSPOSITION_H
+
+#include <stddef.h>
+
+#include "types.h"
+
+typedef enum { EXACT, LOWER, UPPER } Node;
+
+typedef struct {
+    U64 key;
+    int depth;
+    int score;
+    Move best_move;
+    Node node_type;
+} TTentry;
+
+void init_transposition_table(size_t size);
+void tt_store(uint64_t key, int depth, int score, Move best_move, Node node_type);
+TTentry *tt_probe(U64 key);
+void clear_transposition_table(void);
+
+#endif
